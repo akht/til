1365e27e4a6caacd19fdc1c8450b6c16357cef46
akht <picopeko@gmail.com>
2019-03-02 01:35:11 +0900

    ABC065 B
    長くなってしまった

--- kotlin/src/atcoder/tokubeki_sonota/08_ABC065B.kt
+++ kotlin/src/atcoder/tokubeki_sonota/08_ABC065B.kt
@@ -1,2 +1,26 @@
 package atcoder.tokubeki_sonota
 
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val an = (1..n).map { readLine()!!.toInt() - 1 }
+
+    val visited = Array(n) { false }
+    visited[0] = true
+    var no = 0
+    var ans = 0
+    (0 until n).forEach {
+        if (no == 1) {
+            println(ans)
+            return
+        }
+        val next = an[no]
+        if (visited[next]) {
+            println(-1)
+            return
+        }
+        visited[next] = true
+        ans++
+        no = next
+    }
+    println(-1)
+}
