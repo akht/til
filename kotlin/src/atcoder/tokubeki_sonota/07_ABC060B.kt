22fcac9933fd955828340e6b41e56dc01e01c1af
akht <picopeko@gmail.com>
2019-03-02 01:34:52 +0900

    ABC060 B
    無理にかっこよく書こうとしないでシンプルに書く

--- kotlin/src/atcoder/tokubeki_sonota/07_ABC060B.kt
+++ kotlin/src/atcoder/tokubeki_sonota/07_ABC060B.kt
@@ -1,2 +1,11 @@
 package atcoder.tokubeki_sonota
 
+fun main(args: Array<String>) {
+    val (a, b, c) = readLine()!!.split(" ").map { it.toInt() }
+
+    if ((1..b).any { a * it % b == c }) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
