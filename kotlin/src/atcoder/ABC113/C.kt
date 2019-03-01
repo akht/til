1ab63dec91d03e2463d386480fd3155c0513fbb7
akht <picopeko@gmail.com>
2019-03-01 21:48:32 +0900

    ABC113 C
    すげー無駄なことしてた

--- kotlin/src/atcoder/ABC113/C.kt
+++ kotlin/src/atcoder/ABC113/C.kt
@@ -1,35 +1,20 @@
 package atcoder.ABC113
 
-import java.text.DecimalFormat
-
 fun main(args: Array<String>) {
     val (N, M) = readLine()!!.split(" ").map { it.toInt() }
 
-    var xs = mutableListOf<Triple<Long, Long, Long>>()
-    for (i in 0 until M) {
-        val l = readLine()!!.split(" ").map { it.toLong() }
-        val t = Triple(i.toLong(), l[0], l[1])
-        xs.add(t)
+    var xs = (0 until M).map { i ->
+        val l = readLine()!!.split(" ").map { it.toInt() }
+        Triple(i, l[0], l[1])
+    }.sortedBy { it.third }
+
+    val memo = Array(N) { 0 }
+    val ans = Array(M) { "" }
+    for (city in xs) {
+        val p = city.second
+        val n = ++memo[p - 1]
+        ans[city.first] = city.second.toString().padStart(6, '0') + n.toString().padStart(6, '0')
     }
 
-    xs.sortBy { it.third }
-    xs.sortBy { it.second }
-
-    var fmt = DecimalFormat("000000")
-
-    var ans = mutableListOf<Pair<Long, String>>()
-    var pref = xs[0].second
-    var count = 0
-    for (t in xs) {
-        if (pref == t.second) {
-            count++
-        } else {
-            count = 1
-            pref = t.second
-        }
-        var p = Pair(t.first, fmt.format(t.second) + fmt.format(count))
-        ans.add(p)
-    }
-
-    ans.sortedBy { it.first }.forEach { println(it.second) }
+    ans.forEach { println(it) }
 }
