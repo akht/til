6076e138e71cb0f90d27861f14c701941edd9f89
akht <picopeko@gmail.com>
2019-03-10 01:52:03 +0900

    ABC121 D問題 editorial解

--- kotlin/src/atcoder/ABC121/D.kt
+++ kotlin/src/atcoder/ABC121/D.kt
@@ -3,42 +3,15 @@ package atcoder.ABC121
 fun main(args: Array<String>) {
     val (a, b) = readLine()!!.split(" ").map { it.toLong() }
 
-    val keta_b = binaryKetasu(b)
-
-    var ans = 0L
-    for (i in 0 until keta_b) {
-        val now = Math.pow(2.0, i.toDouble()).toLong()
-        val aa = a / now
-        val bb = b / now
-        val q1 = a % now
-        val q2 = b % now
-        if (aa % 2 == 0L && bb % 2 == 0L) {
-            if (i == 0) {
-                var x = (b - a + 1) / 2
-                if (a % 2 != 0L) {
-                    x += 1
-                }
-                if (x % 2 == 1L) {
-                    ans += 1
-                }
-            }
-        } else if (aa % 2 == 0L && bb % 2 != 0L) {
-            if (q2 % 2 == 0L) {
-                ans += now
-            }
-        } else if (aa % 2 != 0L && bb % 2 == 0L) {
-            if (q1 % 2 != 0L) {
-                ans += now
-            }
-        } else {
-            if (Math.abs(q1 - q2) % 2 == 0L) {
-                ans += now
-            }
-        }
-    }
-    println(ans)
+    println(f(a - 1) xor f(b))
 }
 
-fun binaryKetasu(n: Long): Int {
-    return (Math.log10(n.toDouble()) / Math.log10(2.0) + 1).toInt()
+fun f(n: Long): Long {
+    val q = (n + 1) / 2
+    val r = (n + 1) % 2
+    return if (r == 0L) {
+        if (q % 2 == 0L) 0 else 1
+    } else {
+        if (q % 2 == 0L) n else 1L xor n
+    }
 }
