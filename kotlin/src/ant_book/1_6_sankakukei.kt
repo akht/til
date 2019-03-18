8c6a165678c80e3e30d6ab9cdc620c1ae3f0220c
akht <picopeko@gmail.com>
2019-03-18 13:23:52 +0900

    蟻本 1-6 三角形 O(nlogn)解法

--- kotlin/src/ant_book/1_6_sankakukei.kt
+++ kotlin/src/ant_book/1_6_sankakukei.kt
@@ -1,6 +1,17 @@
 package ant_book
 
 fun main(args: Array<String>) {
+
+    // O(n^3)
+//    solve_triple_loop()
+
+    // O(nlogn)でいける
+    solve_nlogn()
+
+}
+
+// O(n^3)
+fun solve_triple_loop() {
     val n = readLine()!!.toInt()
     val an = readLine()!!.split(" ").map { it.toInt() }
 
@@ -22,4 +33,29 @@ fun main(args: Array<String>) {
         }
     }
     println(ans)
+}
+
+// O(nlogn)
+fun solve_nlogn() {
+    val n = readLine()!!.toInt()
+    var an = readLine()!!.split(" ").map { it.toInt() }
+
+    // 辺の配列を降順ソートし、前から最長の辺を決める
+    // 最長の辺をa[i]とした場合、残りの2辺はa[i+1]とa[i+2]を選べばよい
+    // (なぜならソート済なので、それ以降のどの2辺を選んでも和が大きくならないため)
+    // 前から試していって、三角形が作れた時点でそれが答え
+    // ソートにO(nlogn)かかり、調べるのにO(n)かかるので全体としてはO(nlogn)で行える
+
+    an = an.sortedDescending()
+
+    for (i in 0 until an.size - 2) {
+        val len = an[i] + an[i + 1] + an[i + 2]
+        val max = an[i]
+        val rest = len - max
+        if (max < rest) {
+            println(len)
+            return
+        }
+    }
+    println(0)
 }
