e714291d9577c7fc9fc69077cad5dbc99029304d
akht <picopeko@gmail.com>
2019-03-23 15:50:54 +0900

    蟻本 2-2 Fence Repair

--- kotlin/src/ant_book/2_2_Fence_Repair.kt
+++ kotlin/src/ant_book/2_2_Fence_Repair.kt
@@ -1,8 +1,8 @@
 package ant_book
 
 fun main(args: Array<String>) {
-    val n = readLine()!!.toInt()
-    var l = readLine()!!.split(" ").map { it.toLong() }
+    var n = readLine()!!.toInt()
+    var l = readLine()!!.split(" ").map { it.toLong() }.toMutableList()
 
     // 21 -> 13, 8 = 21
     // 13 -> 5,  8 = 13
@@ -44,4 +44,47 @@ fun main(args: Array<String>) {
     //  2 -> 1, 1 = 2
 
 
+    // 板の切り出し方が二分木に対応する
+    // 最適な切り出し方を考えた時、最後に切るのは1番短い板とその次に短い板になるはず
+    // そのひとつ前のステップも同様に、1番短い板とその次に短い板を切り出したはず
+    // というように、ボトムアップで考える
+
+
+    l = l.sorted().toMutableList()
+
+    var ans = 0L
+
+    // 板が1本になるまで
+    while (n > 1) {
+        // 1番短い板mii1, 次に短い板mii2を求める
+        var mii1 = 0
+        var mii2 = 1
+        if (l[mii1] > l[mii2]) {
+            mii1 = 1
+            mii2 = 2
+        }
+
+        for (i in 2 until n) {
+            if (l[i] < l[mii1]) {
+                mii2 = mii1
+                mii1 = i
+            } else if (l[i] < l[mii2]) {
+                mii2 = i
+            }
+        }
+
+        // それらを併合
+        val t = l[mii1] + l[mii2]
+        ans += t
+
+        if (mii1 == n - 1) {
+            val tmp = mii1
+            mii1 = mii2
+            mii2 = tmp
+        }
+        l[mii1] = t;
+        l[mii2] = l[n - 1]
+        n--
+    }
+    println(ans)
 }
