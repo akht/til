11bd2ecd6f8e0445f39312acf8ba09e06f64506b
akht <picopeko@gmail.com>
2019-04-21 00:44:38 +0900

    Tenka1 Programmer Beginner Contest 2019　C問題
    最終形が見えれば累積和

--- kotlin/src/atcoder/Tenka1ProgrammerBeginnerContest2019/C.kt
+++ kotlin/src/atcoder/Tenka1ProgrammerBeginnerContest2019/C.kt
@@ -4,25 +4,50 @@ fun main(args: Array<String>) {
     val n = readLine()!!.toInt()
     val s = readLine()!!
 
-    // 前から#に置き換える
-    val s1 = s.split("").drop(1).take(n).toMutableList()
-    var ans1 = 0
-    for (i in 0 until n - 1) {
-        if (s1[i] == "#" && s1[i + 1] == ".") {
-            ans1++
-            s1[i + 1] = "#"
-        }
+    // 色を変更する操作を行ったあとの最終形としては
+    // 「...###」のように左端から白だけが続いて、ある地点から右端まで黒が続くという形か
+    // もしくは「....」や「####」みたいな1色のみにしかならない
+    // 【例】
+    //    #..# -> ...# = 1回
+    //    .#.# -> ...# = 1回
+    //    #.#. -> .... or #### = 2回
+    // つまり最終形を考えたときに、白から黒に切り替わる境界をどこにするのが最適かを考えればよい
+    // 【例】
+    //    N=4の時、最終形は以下の5つのどれか
+    //      ####
+    //      .###
+    //      ..##
+    //      ...#
+    //      ....
+    //    この中でどれにするのが最適かを考える
+    // 左 left 個が白
+    // 右 N - left 個が黒
+    // として、leftを全探索する。
+    // [0, left)を白にしたい = [0, left)の黒の個数
+    // [left, N)を黒にしたい = [left, N)の白の個数
+    // なので、あらかじめ累積和を用意しておけば高速に求められる
+
+    // 白と黒の個数の累積和を作る
+    val wsum = Array(n + 1) { 0 }
+    val bsum = Array(n + 1) { 0 }
+
+    for (i in 0 until n) {
+        wsum[i + 1] = wsum[i] + (if (s[i] == '.') 1 else 0)
+        bsum[i + 1] = bsum[i] + (if (s[i] == '#') 1 else 0)
     }
 
-    // 後ろから.に置き換える
-    val s2 = s.split("").drop(1).take(n).toMutableList()
-    var ans2 = 0
-    for (i in n - 1 downTo 1) {
-        if (s2[i - 1] == "#" && s2[i] == ".") {
-            ans2++
-            s2[i - 1] = "."
-        }
+    var ans = Int.MAX_VALUE
+    for (left in 0..n) {
+        var tmp = 0
+
+        // leftを全部白にする
+        tmp += bsum[left] - bsum[0]
+
+        // rightを全部黒にする
+        tmp += wsum[n] - wsum[left]
+
+        ans = Math.min(ans, tmp)
     }
 
-    println(Math.min(ans1, ans2))
+    println(ans)
 }
