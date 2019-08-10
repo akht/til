6f63e8e331cffba746bea25a1a168ecf63e85224
akht <picopeko@gmail.com>
2019-08-11 06:11:10 +0900

    ABC137

--- ruby/atcoder/ABC137/d.rb
+++ ruby/atcoder/ABC137/d.rb
@@ -1,3 +1,16 @@
 n, m = gets.strip.split.map(&:to_i)
 abs = n.times.map { gets.strip.split.map(&:to_i) }
 
+abs.sort! {|a, b| (b[1] <=> a[1]).nonzero? || b[0] <=> a[0]}
+days = (1..m).to_a
+
+ans = 0
+abs.each do |day, reward|
+    index = days.bsearch_index { |d| d >= day }
+    if index
+        days.delete_at(index)
+        ans += reward
+    end
+end
+
+puts ans
