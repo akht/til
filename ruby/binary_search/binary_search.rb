954cc3afef54b8603f566002425df25fb68f2fe5
akht <picopeko@gmail.com>
2020-04-14 15:47:31 +0900

    binary search

--- ruby/binary_search/binary_search.rb
+++ ruby/binary_search/binary_search.rb
@@ -30,6 +30,28 @@ def upper_bound(arr, n)
   ok
 end
 
+def bsearch(array, target)
+  left = 0
+  right = array.size - 1
+
+  while left <= right
+    mid = (left + right) / 2
+    if array[mid] == target
+      return mid
+    end
+
+    if array[mid] > target
+      right = mid - 1
+    end
+
+    if array[mid] < target
+      left = mid + 1
+    end
+  end
+
+  -1
+end
+
 class BinarySearchTest < Minitest::Test
   def test_lower_bound
     a = [1, 1, 2, 2, 3, 3, 4, 5]
@@ -51,5 +73,15 @@ class BinarySearchTest < Minitest::Test
     assert_equal 6, upper_bound(a, 3)
     assert_equal 8, upper_bound(a, 5)
     assert_equal 8, upper_bound(a, 100)
+  end
+
+  def test_bsearch
+    a = [-100, 1, 2, 3, 4, 5, 100]
+    assert_equal 0, bsearch(a, -100)
+    assert_equal 1, bsearch(a, 1)
+    assert_equal 2, bsearch(a, 2)
+    assert_equal 5, bsearch(a, 5)
+    assert_equal 6, bsearch(a, 100)
+    assert_equal -1, bsearch(a, 999999)
   end
 end
